927888705164cc2a308e6a5ddb5543acd4f30b58
林泽华 <>
2018-06-21 12:33:21 +0800

    首次提交

---
+++ SpotlightDemo/SpotlightDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpotlightDemo
+//
+//  Created by 林泽华 on 2018/6/21.
+//  Copyright © 2018年 林泽华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpotlightDemo/SpotlightDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpotlightDemo
+//
+//  Created by 林泽华 on 2018/6/21.
+//  Copyright © 2018年 林泽华. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
